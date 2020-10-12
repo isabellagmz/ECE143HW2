31fd52f6679ada81c704c7d0a6be0b63c8956809
isabellagmz <ilgomezt@ucsd.edu>
2020-10-12 15:30:18 -0700

    adding main and powers of three

---
+++ main.py
@@ -0,0 +1,11 @@
+import powers_of_3 as pw
+
+class Homework2:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+
+    my_Homework2 = Homework2()
+    #isi.is_string_integer(some)
+    #snn.compute_sum_to_n(n)
