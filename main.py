a7485eb0d6198311582a3815fd81d252defb2d00
isabellagmz <ilgomezt@ucsd.edu>
2020-10-14 15:54:33 -0700

    nonoverlapping sequences TRY 1. This program does not ensure that the words in the list are not repeated.

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ class Homework2:
 
 if __name__ == '__main__':
     #data = [5, 4, 6, 1, 9, 0, 3, 9, 2, 7, 10, 8, 4, 7, 1, 2, 7, 6, 5, 2, 8, 2, 0, 1, 1, 1, 2, 10, 6, 2]
+
+    words = ["the", "of", "and", "to", "a","in","for","is","on", "that","by","this", "with","i","you",
+             "it","not","or","be","are","from","at","as","your","all","have","new","more","an","was",
+             "we","will","home","can","us","about",'if',"page","my","has",'search',"free","but","our",
+             "one","other","do","no","information"]
     fname = "fname"
-    words = ["list of words"]
+
     my_Homework2 = Homework2()
     nos.write_chunks_of_five(words,fname)
     #wc.write_columns(data, fname)
--- nonoverlapping_sequences.py
+++ nonoverlapping_sequences.py
@@ -9,8 +9,43 @@
 # write_chunks_of_five(words,fname). The words is a list of words from the
 # above corpus and fname is the output filename string.
 
-##############################################################################
-# Creates a file with 10,000 English words arranged in rows of 5             #
-##############################################################################
+import csv
 def write_chunks_of_five(words,fname):
-    pass
+    '''
+    Creates a file with 10,000 English words arranged in rows of 5
+    :param words: list of words that will be arranged
+    :param fname: name of the file that will be created
+    :return: none
+    '''
+
+    # Check words is a list and fname is a string
+    assert type(words) == list
+    assert type(fname) == str
+
+    # Checks that the elements in the list are str
+    for elements in range(len(words)):
+        check_str = isinstance(words[elements], str)
+        assert check_str
+
+    file_entry = "" # formatting string for file
+    words_per_line = 0
+
+    # To arrange incoming words
+    for word in range(len(words)):
+        # Brings counter back to 0 and enters the line after 5 words
+        if words_per_line == 5:
+            words_per_line = 0
+            file_entry = file_entry + "\n"
+        file_entry = file_entry + words[word] + " "
+        words_per_line = words_per_line + 1 # increments counter of words
+    # print(file_entry)
+
+    # remove last space in file
+    file_entry = file_entry[:-1]
+
+    # Making the file
+    with open(fname, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([file_entry])
+
+    return 0
